5e45f8e41fcaf7951ab5f24542c275377ec24a7d
psr <prateek.singh.rathore@gmail.com>
2024-10-08 13:18:00 +0530

    chapter1

---
+++ chapter1/dup.go
@@ -0,0 +1,53 @@
+package chapter1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Dup1() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+		if len(counts) > 3 {
+			fmt.Println(counts)
+			break
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func Dup2() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	}
+	for _, arg := range files {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v]n", err)
+			continue
+		}
+		countLines(f, counts)
+		f.Close()
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
